0bda365b64e740b11745764a0535c809a8f67bf5
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-10-12 22:54:32 +0200

    Add missing exponention of p to n

--- src/finite_field/inversion.py
+++ src/finite_field/inversion.py
@@ -4,11 +4,11 @@ from src.helpers import remove_leading_zeros
 
 
 def inverse(x: list[int], integer_modulus: int, polynomial_modulus: list[int]):
-    # By fermat's little theorem, a^(p-1) = 1 mod p, so from that, we can derive that a^(p-2) = a^-1 mod p
+    # By fermat's little theorem, a^(p^n-1) = 1 mod p, so from that, we can derive that a^(p^n-2) = a^-1 mod p
     remove_leading_zeros(x)
 
-    p = integer_modulus
+    q = integer_modulus ** len(polynomial_modulus) # compute p^n
 
-    # compute x^(p-2) mod p using modular exponentiation
+    # compute x^(q-2) mod p using modular exponentiation
 
     return
