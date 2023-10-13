c78617e2cdd15effc994e06fae2a7a5380157156
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-10-13 18:46:39 +0200

    Remove ws

--- src/finite_field/inversion.py
+++ src/finite_field/inversion.py
@@ -1,5 +1,3 @@
-
-
 from src.helpers import remove_leading_zeros
 
 
@@ -7,7 +5,7 @@ def inverse(x: list[int], integer_modulus: int, polynomial_modulus: list[int]):
     # By fermat's little theorem, a^(p^n-1) = 1 mod p, so from that, we can derive that a^(p^n-2) = a^-1 mod p
     remove_leading_zeros(x)
 
-    q = integer_modulus ** len(polynomial_modulus) # compute p^n
+    q = integer_modulus ** len(polynomial_modulus)  # compute p^n
 
     # compute x^(q-2) mod p using modular exponentiation
 
