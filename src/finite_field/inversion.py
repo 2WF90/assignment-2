1bbc05d8956a3d5160adbe522cff44330e65025f
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-10-15 23:27:03 +0200

    Remove inversion bcs too slow

--- src/finite_field/inversion.py
+++ src/finite_field/inversion.py
@@ -3,10 +3,4 @@ from src.helpers import strip
 
 
 def inverse(f: list[int], integer_modulus: int, polynomial_modulus: list[int]):
-    # By fermat's little theorem, a^(p^n-1) = 1 mod p, so from that, we can derive that a^(p^n-2) = a^-1 mod p
-    q = integer_modulus ** (len(polynomial_modulus) - 1)  # compute p^n
-
-    # compute x^(q-2) mod p using modular exponentiation
-    result = modular_exponentiation(f, q - 2, integer_modulus, polynomial_modulus)
-
-    return result
+    return
