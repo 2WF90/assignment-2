1c13ac6593361845f74b4b0ef8ea5220e93b7e73
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-10-15 23:51:04 +0200

    Add notes to polynomial reduction

--- src/finite_field/polynomial_reduction.py
+++ src/finite_field/polynomial_reduction.py
@@ -1,3 +1,3 @@
-# if degree == degree of polynomial_modulus, subtract the polynomial_modulus from x multiplied by the leading coefficient of x
-# else divide if degree of x is greater than degree of polynomial_modulus?
-# 
+# - if degree == degree of polynomial_modulus, subtract the polynomial_modulus from x multiplied by the leading coefficient of x
+#   else divide if degree of x is greater than degree of polynomial_modulus?
+# - make monic
