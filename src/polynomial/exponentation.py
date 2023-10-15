8a9629f239eb7435cd59f77ea2f12f535ce4c57e
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-10-15 21:41:56 +0200

    Add exponentation implementation

---
+++ src/polynomial/exponentation.py
@@ -0,0 +1,31 @@
+from src.polynomial.multiplication import multiply
+
+
+def modular_exponentiation(f: list[int], power: int, modulus: int) -> list[int]:
+    """
+    Computes the modular exponentiation of a polynomial f raised to a power, modulo a given modulus, using left-to-right
+    binary exponentiation.
+
+    Args:
+        f (list[int]): The polynomial to be raised to a power.
+        power (int): The power to which the polynomial is to be raised.
+        modulus (int): The modulus with respect to which the result is to be computed.
+
+    Returns:
+        list[int]: The result of raising the polynomial f to the power power, modulo the modulus.
+    """
+    if power == 0:
+        return [1]
+
+    if power == 1:
+        return f
+
+    result = [1]
+
+    for bit in bin(power)[2:]:
+        result = multiply(result, result, modulus)
+
+        if bit == "1":
+            result = multiply(result, f, modulus)
+
+    return result
---
+++ tests/unit/exponentation_test.py
@@ -0,0 +1,46 @@
+from src.helpers import strip
+from src.polynomial.exponentation import modular_exponentiation
+
+
+def test_power_2():
+    f = [1, 0, 1, 1]
+
+    assert strip(modular_exponentiation(f, 2, 2)) == [1, 0, 0, 0, 1, 0, 1]
+
+
+def test_power_40():
+    f = [1, 0, 1, 1]
+
+    assert strip(modular_exponentiation(f, 10, 2)) == [
+        1,
+        0,
+        0,
+        0,
+        1,
+        0,
+        1,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        1,
+        0,
+        0,
+        0,
+        1,
+        0,
+        1,
+        0,
+        1,
+        0,
+        0,
+        0,
+        1,
+        0,
+        1,
+    ]
