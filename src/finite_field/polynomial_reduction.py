a18b187ff03941be5a761dd40055235134ee3e01
ThomasNagel <tmj.nagel@gmail.com>
2023-10-25 15:16:08 +0200

    rm: empty file

--- src/finite_field/polynomial_reduction.py
+++
@@ -1,3 +0,0 @@
-# - if degree == degree of polynomial_modulus, subtract the polynomial_modulus from x multiplied by the leading coefficient of x
-#   else divide if degree of x is greater than degree of polynomial_modulus?
-# - make monic
