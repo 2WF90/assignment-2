c7502ae02f5cdfdac8fe85a8eaad7b03b53ba608
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-10-25 15:20:19 +0200

    Implement inversion

--- src/finite_field/inversion.py
+++ src/finite_field/inversion.py
@@ -1,6 +1,19 @@
 from src.finite_field.modular_exponentation import modular_exponentiation
-from src.helpers import strip
+from src.polynomial.xgcd import xgcd
 
 
 def inverse(f: list[int], integer_modulus: int, polynomial_modulus: list[int]):
-    return
+    """
+    Computes the inverse of a polynomial f in a finite field defined by integer_modulus and polynomial_modulus.
+
+    Args:
+        f (list[int]): The polynomial to invert.
+        integer_modulus (int): The integer modulus of the finite field.
+        polynomial_modulus (list[int]): The polynomial modulus of the finite field.
+
+    Returns:
+        list[int]: The inverse of f in the finite field.
+    """
+    _, x, _ = xgcd(f, polynomial_modulus, integer_modulus)
+
+    return x
