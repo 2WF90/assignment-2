17891c34b5018dedb56c0297e8e5102b9c6cf5b4
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-10-26 19:42:47 +0200

    Add surrounding try catch block

--- src/solve.py
+++ src/solve.py
@@ -23,7 +23,10 @@ from src.polynomial.long_division import long_division
 from src.polynomial.multiplication import multiply
 from src.polynomial.subtraction import subtract
 from src.polynomial.xgcd import xgcd
-from src.polynomial.irriducibility_check import irreducibility_check
+from src.polynomial.irreducibility import (
+    generate_irreducible_polynomial,
+    is_irreducible,
+)
 from src.finite_field.primitive import check_primitivity, generate_primitve
 
 
@@ -50,99 +53,110 @@ def solve(exercise: object):
     if integer_modulus < 2:  # disallowed as per definition of assingment
         return {"answer": None}
 
-    if exercise_type == "polynomial_arithmetic":
-        if exercise_task == "addition":
-            a = exercise["f"]
-            b = exercise["g"]
-            result = add(a, b, modulus=integer_modulus)
-            return {"answer": strip(result)}
+    try:
+        if exercise_type == "polynomial_arithmetic":
+            if exercise_task == "addition":
+                a = exercise["f"]
+                b = exercise["g"]
+                result = add(a, b, modulus=integer_modulus)
+                return {"answer": strip(result)}
 
-        if exercise_task == "subtraction":
-            a = exercise["f"]
-            b = exercise["g"]
-            result = subtract(a, b, modulus=integer_modulus)
-            return {"answer": strip(result)}
+            if exercise_task == "subtraction":
+                a = exercise["f"]
+                b = exercise["g"]
+                result = subtract(a, b, modulus=integer_modulus)
+                return {"answer": strip(result)}
 
-        if exercise_task == "multiplication":
-            f = exercise["f"]
-            g = exercise["g"]
-            result = multiply(f, g, integer_modulus)
-            return {"answer": strip(result)}
+            if exercise_task == "multiplication":
+                f = exercise["f"]
+                g = exercise["g"]
+                result = multiply(f, g, integer_modulus)
+                return {"answer": strip(result)}
 
-        if exercise_task == "long_division":
-            f = exercise["f"]
-            g = exercise["g"]
-            try:
-                q, r = long_division(f, g, integer_modulus)
-                return {"answer-q": strip(q), "answer-r": strip(r)}
-            except Exception:
-                return {"answer-q": None, "answer-r": None}
+            if exercise_task == "long_division":
+                f = exercise["f"]
+                g = exercise["g"]
 
-        if exercise_task == "extended_euclidean_algorithm":
-            f = exercise["f"]
-            g = exercise["g"]
-            gcd, x, y = xgcd(f, g, integer_modulus)
-            return {
-                "answer-a": strip(x),
-                "answer-b": strip(y),
-                "answer-gcd": strip(gcd),
-            }
+                try:
+                    q, r = long_division(f, g, integer_modulus)
+                    return {"answer-q": strip(q), "answer-r": strip(r)}
+                except Exception:
+                    return {"answer-q": None, "answer-r": None}
 
-        if exercise_task == "irreducibility_check":
-            f = exercise["f"]
-            return {"answer": irreducibility_check(f, modulus=integer_modulus)}
+            if exercise_task == "extended_euclidean_algorithm":
+                f = exercise["f"]
+                g = exercise["g"]
 
-    elif exercise_type == "finite_field_arithmetic":
-        polynomial_modulus = exercise["polynomial_modulus"]
+                try:
+                    gcd, x, y = xgcd(f, g, integer_modulus)
 
-        if exercise_task == "addition":
-            f = exercise["f"]
-            g = exercise["g"]
-            result = add(f, g, modulus=integer_modulus)
-            _, rem = long_division(result, polynomial_modulus, integer_modulus)
-            return {"answer": strip(rem)}
+                    return {
+                        "answer-a": strip(x),
+                        "answer-b": strip(y),
+                        "answer-gcd": strip(gcd),
+                    }
+                except:
+                    return {"answer-gcd": None, "answer-x": None, "answer-y": None}
 
-        if exercise_task == "subtraction":
-            f = exercise["f"]
-            g = exercise["g"]
-            result = subtract(f, g, modulus=integer_modulus)
-            _, rem = long_division(result, polynomial_modulus, integer_modulus)
-            return {"answer": strip(rem)}
+            if exercise_task == "irreducibility_check":
+                f = exercise["f"]
+                return {"answer": is_irreducible(f, modulus=integer_modulus)}
 
-        if exercise_task == "multiplication":
-            f = exercise["f"]
-            g = exercise["g"]
-            result = multiply(f, g, integer_modulus)
-            _, rem = long_division(result, polynomial_modulus, integer_modulus)
-            return {"answer": strip(rem)}
+            if exercise_task == "irreducible_element_generation":
+                modulus = exercise["integer_modulus"]
+                degree = exercise["degree"]
+                return {"answer": generate_irreducible_polynomial(degree, modulus)}
 
-        if exercise_task == "inversion":
-            f = exercise["f"]
-            result = inverse(f, integer_modulus, polynomial_modulus)
-            _, rem = long_division(result, polynomial_modulus, integer_modulus)
-            return {"answer": strip(rem)}
+        elif exercise_type == "finite_field_arithmetic":
+            polynomial_modulus = exercise["polynomial_modulus"]
 
-        if exercise_task == "division":
-            f = exercise["f"]
-            g = exercise["g"]
-            try:
+            if exercise_task == "addition":
+                f = exercise["f"]
+                g = exercise["g"]
+                result = add(f, g, modulus=integer_modulus)
+                _, rem = long_division(result, polynomial_modulus, integer_modulus)
+                return {"answer": strip(rem)}
+
+            if exercise_task == "subtraction":
+                f = exercise["f"]
+                g = exercise["g"]
+                result = subtract(f, g, modulus=integer_modulus)
+                _, rem = long_division(result, polynomial_modulus, integer_modulus)
+                return {"answer": strip(rem)}
+
+            if exercise_task == "multiplication":
+                f = exercise["f"]
+                g = exercise["g"]
+                result = multiply(f, g, integer_modulus)
+                _, rem = long_division(result, polynomial_modulus, integer_modulus)
+                return {"answer": strip(rem)}
+
+            if exercise_task == "inversion":
+                f = exercise["f"]
+                result = inverse(f, integer_modulus, polynomial_modulus)
+                _, rem = long_division(result, polynomial_modulus, integer_modulus)
+                return {"answer": strip(rem)}
+
+            if exercise_task == "division":
+                f = exercise["f"]
+                g = exercise["g"]
                 result = division(f, g, polynomial_modulus, integer_modulus)
                 return {"answer": strip(result)}
-            except Exception:
-                return {"answer": None}
 
-        if exercise_task == "primitivity_check":
-            f = exercise["f"]
-            poly_mod = exercise["polynomial_modulus"]
-            modulus = exercise["integer_modulus"]
-            return {"answer": check_primitivity(f, poly_mod, modulus)}
+            if exercise_task == "primitivity_check":
+                f = exercise["f"]
+                poly_mod = exercise["polynomial_modulus"]
+                modulus = exercise["integer_modulus"]
+                return {"answer": check_primitivity(f, poly_mod, modulus)}
 
-        if exercise_task == "primitive_element_generation":
-            poly_mod = exercise["polynomial_modulus"]
-            modulus = exercise["integer_modulus"]
-            return {"answer": generate_primitve(poly_mod, modulus)}
+            if exercise_task == "primitive_element_generation":
+                poly_mod = exercise["polynomial_modulus"]
+                modulus = exercise["integer_modulus"]
+                return {"answer": generate_primitve(poly_mod, modulus)}
 
-    return {"answer": None}
+        raise ValueError("Invalid exercise type or task")
+    except Exception:
+        return {"answer": None}
 
 
 def solve_from_file(exercise_location: str):
