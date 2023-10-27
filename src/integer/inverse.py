f3a4785dabca34d3d545639fafde4c8efdfb2cb7
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-10-27 18:32:41 +0200

    Implement int xgcd from number theory script

--- src/integer/inverse.py
+++ src/integer/inverse.py
@@ -1,10 +1,27 @@
 def extended_gcd(a: int, b: int):
-    if a == 0:
-        return (b, 0, 1)
+    a_prime = a
+    b_prime = b
+    x_1, x_2, y_1, y_2 = 1, 0, 0, 1
 
-    gcd, x, y = extended_gcd(b % a, a)
+    while b_prime > 0:
+        q = a_prime // b_prime
+        r = a_prime - q * b_prime
 
-    return (gcd, y - (b // a) * x, x)
+        a_prime = b_prime
+        b_prime = r
+
+        x_3 = x_1 - q * x_2
+        y_3 = y_1 - q * y_2
+
+        x_1, x_2 = x_2, x_3
+        y_1, y_2 = y_2, y_3
+
+    d = a_prime
+
+    x = x_1 if a >= 0 else -x_1
+    y = y_1 if b >= 0 else -y_1
+
+    return d, x, y
 
 
 def integer_inverse(x: int, modulus: int):
